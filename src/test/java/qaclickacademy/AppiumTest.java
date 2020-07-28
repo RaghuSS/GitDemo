a886268ec258301d7a0733aab277f7ceeaa47a72
RaghuSS <Raghu.s110@gmail.com>
2020-07-28 21:37:53 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeappAndroid()
+	{
+		System.out.println("NativeappAndroid");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
